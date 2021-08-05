61a7d7197d84827f2271a7c80dfa491d50ed779f
Yamini-byte <56766286+Yamini-byte@users.noreply.github.com>
2021-08-05 13:52:09 +0530

    Time: 1 ms (92.56%), Space: 38.7 MB (76.17%) - LeetHub

---
+++ maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProduct(int[] A) {
+        //calculate the left subarray product and right subarray product for each element and return the max;
+         int n = A.length, res = A[0], l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            l =  (l == 0 ? 1 : l) * A[i];
+            r =  (r == 0 ? 1 : r) * A[n - 1 - i];
+            res = Math.max(res, Math.max(l, r));
+        }
+        return res;
+    }
+}
