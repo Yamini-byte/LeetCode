7e0ff6e676279f0cb5196b17abd3aeab7ca83124
Yamini-byte <56766286+Yamini-byte@users.noreply.github.com>
2021-08-05 23:24:48 +0530

    Time: 0 ms (100.00%), Space: 36.9 MB (92.88%) - LeetHub

---
+++ maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxDepth(String s) {
+        int res = 0, cur = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '('){
+                res = Math.max(res,++cur);
+            }
+            else if(s.charAt(i) == ')'){
+                --cur;
+            }
+        }
+        return res;
+    }
+}
